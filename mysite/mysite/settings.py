f80ff10856f7d823dcbf633be420d6e30991a462
muztishini <111636144+muztishini@users.noreply.github.com>
2023-11-13 15:39:23 +0500

    v2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -81,16 +81,30 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'djangopostgre',
-        'USER': 'testuser',
-        'PASSWORD': '123456',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'djangopostgre',
+#         'USER': 'testuser',
+#         'PASSWORD': '123456',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
+DATABASES = { 
+    'default': { 
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'djangomysql', 
+        'USER': 'root', 
+        'PASSWORD': '12345', 
+        'HOST': '127.0.0.1', 
+        'PORT': '3306', 
+        'OPTIONS': { 
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'" 
+        } 
+    } 
+} 
 
 
 # Password validation
